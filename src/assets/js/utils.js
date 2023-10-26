9ff5c0d591422c14db51cf8f753eea3edbc4c15e
Aaron Peña <aaronpena104@gmail.com>
2023-10-26 08:53:25 -0700

    Fecha arreglada
    Devuelve la fecha en formato MM/DD/YYYY

--- src/assets/js/utils.js
+++ src/assets/js/utils.js
@@ -4,14 +4,16 @@ function formatDate(date) {
       throw new Error('El valor proporcionado no es una instancia de Date.');
     }
   
+
     const day = date.getUTCDate();
-    const month = date.getUTCMonth() + 1; // Sumamos 1 ya que los meses son base 0.
+    const month = date.getUTCMonth()+1; 
     const year = date.getUTCFullYear();
   
     // Aseguramos que los componentes de la fecha tengan al menos dos dígitos.
     const formattedDay = String(day).padStart(2, '0');
     const formattedMonth = String(month).padStart(2, '0');
+    
   
-    return `${formattedDay}/${formattedMonth}/${year}`;
+    return `${formattedMonth}/${formattedDay}/${year}`;
   }
   
